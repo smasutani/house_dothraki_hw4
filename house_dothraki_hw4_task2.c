a39c709708f8f1cf819eaf23e34c6d15c0f0c5e2
Sherry Masutani <sherrymasutani@mail.weber.edu>
2017-02-14 22:17:53 +0000

    initial commit

---
+++ house_dothraki_hw4_task2.c
@@ -0,0 +1,35 @@
+/*
+ * =====================================================================================
+ *
+ *       Filename:  house_dothraki_hw4_task2.c
+ *       	Usage:  ./house_dothraki_hw4_task2.c
+ *    Description:  
+ *
+ *        Version:  1.0
+ *        Created:  02/14/2017 10:16:46 PM
+ *       Compiler:  gcc -Wall -Werr
+ *         Author:  Sherry Masutani (), sherrymasutani@mail.weber.edu
+ * =====================================================================================
+ */
+#include <stdio.h>
+#define GAME 3
+#define BOWLER 5
+
+int main()
+{
+	float game, total;
+
+	for (int i = 1; i <= BOWLER ; i++)
+	{
+		for (int j = 1; j <= GAME ; j++)
+		{
+			printf("Enter the score for game %d for bowler %d: ", j, i);
+			scanf("%f", &game);
+			total += game;
+		}
+
+		printf("The average for bowler %d is %f\n", i, total/3);
+
+	}
+	return 0;
+}
